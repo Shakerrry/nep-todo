ff5e8fdc28ee27272f9631f0a7fc184834351082
stefan petrea <petreastefanvalentin@yahoo.com>
2023-01-16 13:37:08 +0200

    validation test

--- resources/js/store/modules/task/mutations.js
+++ resources/js/store/modules/task/mutations.js
@@ -5,34 +5,3 @@ export const SET_TASKS = (state, tasks) => {
 export const SET_TASK = (state, task) => {
     state.task = task;
 }
-
-// export const ADD_TO_CART = (state, { product, quantity }) => {
-//
-//     let productInCart = state.cart.find(item => {
-//         return item.product.id === product.id;
-//     });
-//
-//     if (productInCart) {
-//         productInCart.quantity += quantity;
-//         return;
-//     }
-//
-//     state.cart.push({
-//         product,
-//         quantity
-//     })
-// }
-//
-// export const SET_CART = (state, cartItems) => {
-//     state.cart = cartItems;
-// }
-//
-// export const REMOVE_PRODUCT_FROM_CART = (state, product) => {
-//     state.cart = state.cart.filter(item => {
-//         return item.product.id !== product.id;
-//     })
-// }
-//
-// export const CLEAR_CART_ITEMS = (state) => {
-//     state.cart = [];
-// }
